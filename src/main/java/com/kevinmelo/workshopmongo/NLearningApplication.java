e49da13cb1aea6cde1d56bd705e0e80167fd24bf
kevingabrielmelo <kevinalvesmelo@gmail.com>
2021-03-15 13:59:49 -0300

    Upando o projeto vazio para o Github

---
+++ src/main/java/com/kevinmelo/workshopmongo/NLearningApplication.java
@@ -0,0 +1,13 @@
+package com.kevinmelo.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kevinmelo/workshopmongo/NLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevinmelo.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
