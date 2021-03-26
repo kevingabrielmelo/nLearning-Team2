e0ad8524d6dc0a1d2d4089c57a8ef07d063470bc
kevingabrielmelo <kevinalvesmelo@gmail.com>
2021-03-26 13:38:51 -0300

    bugfix de email e senha aparecendo na rota do login

--- src/main/java/com/nlearning/controllers/LoginController.java
+++ src/main/java/com/nlearning/controllers/LoginController.java
@@ -2,8 +2,12 @@ package com.nlearning.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 // imports de admin
 import com.nlearning.models.Admin;
@@ -21,6 +25,7 @@ import com.nlearning.repository.TutorRepository;
 
 @Controller
 public class LoginController {
+
 	// Chamando os repositórios para pesquisa no banco para o login
 	@Autowired
 	private AdminRepository adminRepository;
@@ -30,17 +35,27 @@ public class LoginController {
 	private TutorRepository tutorRepository;
 	@Autowired
 	private AlunoRepository alunoRepository;
-    //Deslogar o usuário (usar esse mapping para deslogar)
+
+	// Deslogar o usuário (usar esse mapping para deslogar)
 	@GetMapping("/login")
 	public String sair() {
-			Usuario.tipoUsu="";
-			Usuario.idUsu = null;
+		Usuario.tipoUsu = "";
+		Usuario.idUsu = null;
 		return "redirect:Login";
 	}
 
-	@GetMapping("Login")
+	// Requisição da página de login
+	@RequestMapping(value = "Login", method = RequestMethod.GET)
+	public ModelAndView dadosAluno(Long idAluno) {
+		ModelAndView mv = new ModelAndView("login");
+		return mv;
+	}
+
+	// Executa todos os passos do login do usuário desejado
+	@ExceptionHandler
+	@PostMapping("Login")
 	public String login(String email, String senha) {
-		//Criando classes para efetuação do login
+		// Criando classes para efetuação do login
 		Admin admin = new Admin();
 		admin = adminRepository.findByEmail(email);
 		Gestor gestor = new Gestor();
@@ -49,53 +64,49 @@ public class LoginController {
 		tutor = tutorRepository.findByEmail(email);
 		Aluno aluno = new Aluno();
 		aluno = alunoRepository.findByEmail(email);
-		
-		//Checando no banco se o email existe
-		if(admin != null || gestor != null || tutor != null || aluno != null) 
-		{
-			//Verificando em cada tabela se o emais e a senha no banco são iguais as salvas no banco
-			if(admin != null && admin.getEmail().equals(email) && admin.getSenha().equals(senha))
-			{
+
+		// Checando no banco se o email existe
+		if (admin != null || gestor != null || tutor != null || aluno != null) {
+			// Verificando em cada tabela se o emais e a senha no banco são iguais as salvas
+			// no banco
+			if (admin != null && admin.getEmail().equals(email) && admin.getSenha().equals(senha)) {
 				Usuario.tipoUsu = "admin";
 				Usuario.idUsu = admin.getIdAdmin();
 				return "redirect:menuAdmin";
-			}
-			else if(gestor != null && gestor.getEmail().equals(email) && gestor.getSenha().equals(senha)) 
-			{
+			} else if (gestor != null && gestor.getEmail().equals(email) && gestor.getSenha().equals(senha)) {
 				Usuario.tipoUsu = "gestor";
 				Usuario.idUsu = gestor.getIdGestor();
 				return "redirect:menuGestor";
-			}
-			else if(tutor != null && tutor.getEmail().equals(email) && tutor.getSenha().equals(senha)) 
-			{
+			} else if (tutor != null && tutor.getEmail().equals(email) && tutor.getSenha().equals(senha)) {
 				Usuario.tipoUsu = "tutor";
 				Usuario.idUsu = tutor.getIdTutor();
 				return "redirect:menuTutor";
-			}
-			else if(aluno != null && aluno.getEmail().equals(email) && aluno.getSenha().equals(senha)) 
-			{
+			} else if (aluno != null && aluno.getEmail().equals(email) && aluno.getSenha().equals(senha)) {
 				Usuario.tipoUsu = "aluno";
 				Usuario.idUsu = aluno.getIdAluno();
 				return "redirect:menuAluno";
 			}
 		}
-		//caso não encontre nenhum email	
-		else 
-		{
+		// caso não encontre nenhum email
+		else {
 			return null;
 		}
 		return null;
 	}
-	
-	//Verifica qual o usuário para voltar ao menu correto
+
+	// Verifica qual o usuário para voltar ao menu correto
 	@RequestMapping("voltar")
-	public String voltar() 
-	{
-		if(Usuario.tipoUsu == "admin") { return "redirect:menuAdmin"; }
-		else if (Usuario.tipoUsu == "gestor") { return "redirect:menuGestor"; }
-		else if (Usuario.tipoUsu == "tutor") { return "redirect:menuTutor"; }
-		else if (Usuario.tipoUsu == "aluno") { return "redirect:menuAluno"; }
+	public String voltar() {
+		if (Usuario.tipoUsu == "admin") {
+			return "redirect:menuAdmin";
+		} else if (Usuario.tipoUsu == "gestor") {
+			return "redirect:menuGestor";
+		} else if (Usuario.tipoUsu == "tutor") {
+			return "redirect:menuTutor";
+		} else if (Usuario.tipoUsu == "aluno") {
+			return "redirect:menuAluno";
+		}
 		return null;
 	}
-  
+
 }
