4b7f8d6dbcb1c7a1c3897d5362e8fffd60669a5f
kevingabrielmelo <kevinalvesmelo@gmail.com>
2021-04-02 11:53:28 -0300

    Inserção de imagens no banco (já cadastra, e não exibe)

--- src/main/java/com/nlearning/controllers/CursoController.java
+++ src/main/java/com/nlearning/controllers/CursoController.java
@@ -1,31 +1,57 @@
 package com.nlearning.controllers;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.nlearning.models.Curso;
+import com.nlearning.models.CursoControllerModel;
 import com.nlearning.repository.CursoRepository;
 
 @Controller
 public class CursoController {
-	
+
 	@Autowired
 	private CursoRepository cursoRepository;
-	
+
 	// Rota para o get do form
 	@RequestMapping(value = "/cadastrarCurso", method = RequestMethod.GET)
 	public String form() {
-
 		return "curso/form_curso";
-
 	}
 
-	// Cadastra os dados do aluno no banco de dados
-	@RequestMapping(value = "/cadastrarCurso", method = RequestMethod.POST)
-	public String form(Curso curso) {
-			cursoRepository.save(curso);
-			return "redirect:cadastrarCurso";
-		}
+	// Cadastra os dados do curso no banco de dados
+	@RequestMapping(value = "/cadastrarCurso", method = RequestMethod.POST, consumes = { "multipart/form-data" })
+	public String form(@RequestParam(value = "imagem") MultipartFile imagem, CursoControllerModel curso)
+			throws IOException {
+		cursoRepository.save(CursoMapper.converter(curso, imagem));
+		return "redirect:cadastrarCurso";
 	}
+
+	@GetMapping(value = "/cursos")
+	public ModelAndView listaCursos() throws UnsupportedEncodingException {
+		ModelAndView mv = new ModelAndView("/curso/lista_cursos");
+		Iterable<Curso> curso = cursoRepository.findAll();
+		mv.addObject("curso", curso);
+
+		return mv;
+	}
+
+	@GetMapping(value = "/cursos/{id}")
+	public ResponseEntity<Curso> listCursos(@PathVariable("id_curso") Long id_curso) {
+		Curso curso = cursoRepository.findByIdCurso(id_curso);
+		// ModelAndView mv = new ModelAndView("/curso/lista_cursos");
+		return new ResponseEntity<Curso>(curso, HttpStatus.OK);
+	}
+}
---
+++ src/main/resources/static/js/carregarImagem.js
@@ -0,0 +1,7 @@
+function converteImagem() {
+	
+	imagem = document.getElementById('img').value;
+	
+	var src = "data:image/png;base64,";
+	src += imagem;
+}
