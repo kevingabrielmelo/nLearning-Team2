ce08cdf7f97444ddabfd2263642725970816d69a
kevingabrielmelo <kevinalvesmelo@gmail.com>
2021-04-12 17:00:54 -0300

    Lista de cursos finalizada

--- src/main/java/com/nlearning/controllers/CursoController.java
+++ src/main/java/com/nlearning/controllers/CursoController.java
@@ -16,10 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.nlearning.models.Curso;
-import com.nlearning.models.CursoAluno;
 import com.nlearning.models.CursoControllerModel;
-import com.nlearning.models.Usuario;
-import com.nlearning.repository.CursoAlunoRepository;
 import com.nlearning.repository.CursoRepository;
 
 @Controller
@@ -27,9 +24,6 @@ public class CursoController {
 
 	@Autowired
 	private CursoRepository cursoRepository;
-	
-	@Autowired
-	private CursoAlunoRepository cursoAlunoRepository;
 
 	// Rota para o get do form
 	@RequestMapping(value = "/cadastrarCurso", method = RequestMethod.GET)
--- src/main/java/com/nlearning/repository/CursoAlunoRepository.java
+++ src/main/java/com/nlearning/repository/CursoAlunoRepository.java
@@ -5,7 +5,9 @@ import org.springframework.data.repository.CrudRepository;
 import com.nlearning.models.CursoAluno;
 
 public interface CursoAlunoRepository extends CrudRepository<CursoAluno, String>{
-	//Procura Aluno por ID
-	CursoAluno findByIdCursoAluno(Long codigo);
+	//Procura Aluno por ID do CursoAluno
+	Iterable<CursoAluno> findAllByIdCursoAluno(Long codigo);
+	//Procura Aluno por ID do aluno
+	Iterable<CursoAluno> findAllByIdAluno(Long codigo);
 
 }
